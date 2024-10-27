d6aaef662b3e94230d3b742d4934e43afdd82fe8
f0reachARR <f0reach@f0reach.me>
2024-10-27 23:40:04 +0900

    Add expect output

--- testcases.py
+++ testcases.py
@@ -13,11 +13,20 @@ def get_testcase(testsuite: str, basename: str):
     return path.read_text()
 
 
+def get_testcase_expect(testsuite: str, basename: str, type: str):
+    path = TEST_CASE_DIR / testsuite / "test_expects" / f"{basename}.{type}"
+    if not path.exists():
+        return ""
+    return path.read_text()
+
+
 @dataclass
 class TestcasePair:
     name: str
     test_input: str
     test_output: str
+    test_expect_stdout: str
+    test_expect_stderr: str
 
 
 def create_testcase_result_pair(testsuite: str, result_dir: Path) -> List[TestcasePair]:
@@ -27,6 +36,16 @@ def create_testcase_result_pair(testsuite: str, result_dir: Path) -> List[Testca
         basename = output.stem
         test_input = get_testcase(testsuite, basename)
         test_output = output.read_text()
-        pairs.append(TestcasePair(basename, test_input, test_output))
+        test_expect_stdout = get_testcase_expect(testsuite, basename, "stdout")
+        test_expect_stderr = get_testcase_expect(testsuite, basename, "stderr")
+        pairs.append(
+            TestcasePair(
+                basename,
+                test_input,
+                test_output,
+                test_expect_stdout,
+                test_expect_stderr,
+            )
+        )
 
     return pairs
